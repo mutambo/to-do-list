889d08187aceba9a4fe610f736ca5335e23f2b28
mutambo <55140441+mutambo@users.noreply.github.com>
2022-02-02 23:34:28 +0300

    created a todo header

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -10,13 +10,13 @@ function TodoForm (props) {
   const handleSubmit = e => {
       e.preventDefault();
 
-    //   this.props.onSubmit({
+    //   props.onSubmit({
     //       id:Math.floor(Math.random() * 10000),
     //       text: input
     //   });
       setInput = ('')
   };
-
+ 
   return (
     <form className="todo-form" onSubmit ={handleSubmit}>
       <input type='text' placeholder='Add a todo' value={input} name = 'text' className = 'todo-input' onChange= {handleChange} />
