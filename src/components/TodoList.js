3615e11f140f913b99533d3bfad65d316fe4d571
mutambo <55140441+mutambo@users.noreply.github.com>
2022-02-03 17:13:17 +0300

    added udate todo function in Todo

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -14,11 +14,20 @@ function TodoList() {
         setTodos(newTodos);
     };
 
+    const updateTodo = (todoId, newValue) =>{
+        if(!newValue.text || /^\s*$/.test(newValue.text)){
+            return
+        } 
+        setTodos(prev => prev.map(item=>(item.id === todoId ? newValue: item))) 
+    }
+
     const removeTodo = id => {
         const removeArr = [...todos].filter(todo => todo.id !==id)
 
         setTodos(removeArr)
-    }
+    };
+
+    
 
     const completeTodo = id => {
         let updatedTodos = todos.map(todo => {
@@ -35,7 +44,7 @@ function TodoList() {
        <h1>What's the plan for Today</h1>
       <TodoForm onSubmit ={addTodo}/>
       <Todo
-          todos = {todos} completeTodo = {completeTodo} removeTodo ={removeTodo}
+          todos = {todos} completeTodo = {completeTodo} removeTodo ={removeTodo} updateTodo = {updateTodo}
       />
     </div>
   );
