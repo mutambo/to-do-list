4f0fa0b5260d4d4b459a20b26aefa3c83be9c7a5
mutambo <55140441+mutambo@users.noreply.github.com>
2022-02-03 18:06:45 +0300

    added css file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 // import TodoForm from './components/TodoForm';
 import TodoList from './components/TodoList';
-
+import './App.css'
 function App() {
   return (
     <div className = 'todo-app'>
