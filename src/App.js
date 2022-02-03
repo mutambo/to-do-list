0a70889f4162dcab22d19843a56e2ca85fc898da
mutambo <55140441+mutambo@users.noreply.github.com>
2022-02-03 16:29:30 +0300

    fixed bugs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
 
 function App() {
   return (
     <div className = 'todo-app'>
-     <TodoForm/>
+     {/* <TodoForm/> */}
+     <TodoList/>
     </div>
   );
 
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,15 +1,28 @@
 import React, {useState} from 'react';
 import TodoForm from './TodoForm'
-function Todo() {
+import TodoList from './TodoList';
+import {RiCloseCircleLine} from 'react-icons/ri'
+import {TiEdit} from 'react-icons/ti'
 
-    const [todos, setTodos] = useState([])
-  return (
-    <div>
-      <h1>What's the plan for Today</h1>
-      <TodoForm/>
+function Todo({todos, completeTodo}) {
+    const [edit, setEdit] =useState({
+        id: null,
+        value:''
+    })
+
+
+  return todos.map((todo, index) =>(
+   <div className = {todo.isComplete ? 'todo-row complete' : 'todo-row'} key= {index}>
+    <div key = {todo.id} onClick = {()=>completeTodo(todo.id)}>
+      {todo.text}  
     </div>
-  );
-
+    <div className= "icons">
+      <RiCloseCircleLine/>  
+      <TiEdit/>
+    </div>
+   </div>   
+  ))
 }
 
 export default Todo;
+
